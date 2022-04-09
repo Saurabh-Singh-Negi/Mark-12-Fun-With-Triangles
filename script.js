c14c5b5bbd2a1ade0af7d62145605cad3866ac56
Saurabh-Singh-Negi <saurabhnegi0007@gmail.com>
2022-04-09 16:56:29 +0530

    added selectors in JS file for html elements

---
+++ script.js
@@ -0,0 +1,11 @@
+var Angle1 = document.querySelector("#angle1");
+var Angle2 = document.querySelector("#angle2");
+var Angle3 = document.querySelector("#angle3");
+var btnCheck = document.querySelector("#check-triangle");
+var outputMessage = document.querySelector("#output");
+
+function isTriangle() {
+    
+}
+
+btnCheck.addEventListener("click", isTriangle);
