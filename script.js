7e31092b9b2ff2a7f0ce077bca1dad195ebf3d9b
Saurabh-Singh-Negi <saurabhnegi0007@gmail.com>
2022-04-09 17:16:20 +0530

    funtionality to check if triangle or not

--- script.js
+++ script.js
@@ -4,8 +4,20 @@ var Angle3 = document.querySelector("#angle3");
 var btnCheck = document.querySelector("#check-triangle");
 var outputMessage = document.querySelector("#output");
 
+
+function calculateSumOfAngles(angleOne,angleTwo,angleThree) {
+    var sum = Number(angleOne)+Number(angleTwo)+Number(angleThree);
+    return sum; 
+}
+
 function isTriangle() {
-    
+    var sumOfAngles = calculateSumOfAngles(Angle1.value,Angle2.value,Angle3.value);
+    if(Number(sumOfAngles) == 180) {
+        outputMessage.innerText = "Yay! That's a triangle";
+    }
+    else {
+        outputMessage.innerText = "Sorry! That's not a triangle";
+    }
 }
 
 btnCheck.addEventListener("click", isTriangle);
