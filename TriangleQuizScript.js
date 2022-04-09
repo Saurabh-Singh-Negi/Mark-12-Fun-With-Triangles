21ab3a9fe44debd48894b1b19612379fd93e3118
Saurabh-Singh-Negi <saurabhnegi0007@gmail.com>
2022-04-09 18:52:35 +0530

    functionality to check score

--- TriangleQuizScript.js
+++ TriangleQuizScript.js
@@ -1,3 +1,20 @@
-const quizForm = document.querySelector("#quiz-form");
+const quizForm = document.querySelector(".quiz-form");
 const btnCheck = document.querySelector("#submit-answers");
 const outputMsg = document.querySelector("#output");    
+
+const correctAnswers = ['90', 'Right Angled'];
+
+function calculateScore() {
+    var score = 0;
+    var index = 0;
+    const data = new FormData(quizForm);
+    for(let answer of data.values()) {
+        if(answer === correctAnswers[index]) {
+            score++;
+        }
+        index++;
+    }
+    outputMsg.innerText = "score is:" + score;
+}
+
+btnCheck.addEventListener("click", calculateScore);
