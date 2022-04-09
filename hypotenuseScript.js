a43a17d7e690dd5b546441e236789f2d49f172f7
Saurabh-Singh-Negi <saurabhnegi0007@gmail.com>
2022-04-09 19:07:51 +0530

    query selector added

---
+++ hypotenuseScript.js
@@ -0,0 +1,3 @@
+const sideLength = document.querySelectorAll(".side-length");
+const btnCalculate = document.querySelector("calculate-button");
+const outputMessage = document.querySelector("#output");
