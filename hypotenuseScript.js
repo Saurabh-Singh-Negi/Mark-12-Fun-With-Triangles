41c3872e3efafd6e65cb431925612f350d0443dd
Saurabh-Singh-Negi <saurabhnegi0007@gmail.com>
2022-04-09 23:33:24 +0530

    functionality for finding length

--- hypotenuseScript.js
+++ hypotenuseScript.js
@@ -1,3 +1,18 @@
 const sideLength = document.querySelectorAll(".side-length");
-const btnCalculate = document.querySelector("calculate-button");
+const btnCalculate = document.querySelector("#calculate-button");
 const outputMessage = document.querySelector("#output");
+
+function calculateSquare(side1,side2) {
+    var sum = side1*side1 + side2*side2;
+    return sum;
+}
+
+
+function calculateHypotenuse() {
+    var sumOfSquares = calculateSquare(Number(sideLength[0].value),Number(sideLength[1].value));
+    var result = Math.sqrt(sumOfSquares);  
+    outputMessage.innerText = "result:" + result; 
+}
+
+
+btnCalculate.addEventListener("click", calculateHypotenuse);
